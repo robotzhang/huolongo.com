93eba1c0909840fa5b483be3b9e2a5b05f3ed2e8
robotzhang <445679586@qq.com>
2013-03-03 16:26:36 +0800

    init 目录结构

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="zh_CN">
+ <head>
+  <meta charset="utf-8">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>火龙果网络科技有限公司</title>  
+ </head>
+
+ <body>
+  <div>
+    欢迎！
+  </div>
+ </body>
+</html>
