3fff07ddc1192850fcea9538cbd2ba3a4e1cca88
robotzhang <445679586@qq.com>
2013-03-03 19:02:37 +0800

    首页

--- index.php
+++ index.php
@@ -6,11 +6,42 @@
   <meta name="Keywords" content="">
   <meta name="Description" content="">
   <title>火龙果网络科技有限公司</title>  
+  <link href="http://lib.sinaapp.com/js/bootstrap/2.2.1/css/bootstrap.min.css"" media="screen" rel="stylesheet" type="text/css">
+  <link href="http://lib.sinaapp.com/js/bootstrap/2.2.1/css/bootstrap-responsive.min.css"" media="screen" rel="stylesheet" type="text/css">
+  <script src="http://lib.sinaapp.com/js/jquery/1.9.0/jquery.min.js"></script>
  </head>
 
  <body>
+  <div class="header">
+    <div class="container">
+    <h1 class="logo">
+      火龙果
+	</h1>
+	<div class="pull-right">
+	  <ul>
+	    <li>
+		  <a href="">首页</a>
+		</li>
+		<li>
+		  <a href="">案例</a>
+		</li>
+		<li>
+		  <a href="">报价</a>
+		</li>
+		<li>
+		  <a href="">联系</a>
+		</li>
+	  </ul>
+	</div>
+	</div>
+  </div>
   <div>
-    欢迎！
+    <div class="container">欢迎！</div>
+  </div>
+  <div class="footer">
+	<div class="container">
+	  Copyright 2013 火龙果
+	</div>
   </div>
  </body>
 </html>
