981d6676e89948ac61c854e185850d0248628a1e
robotzhang <445679586@qq.com>
2013-03-09 21:08:09 +0800

    还是文案

--- index.php
+++ index.php
@@ -6,8 +6,9 @@
   <meta name="Keywords" content="">
   <meta name="Description" content="">
   <title>火龙果网络科技有限公司</title>  
-  <link href="http://lib.sinaapp.com/js/bootstrap/2.2.1/css/bootstrap.min.css"" media="screen" rel="stylesheet" type="text/css">
-  <link href="http://lib.sinaapp.com/js/bootstrap/2.2.1/css/bootstrap-responsive.min.css"" media="screen" rel="stylesheet" type="text/css">
+  <link href="http://lib.sinaapp.com/js/bootstrap/2.2.1/css/bootstrap.min.css" media="screen" rel="stylesheet" type="text/css">
+  <link href="http://lib.sinaapp.com/js/bootstrap/2.2.1/css/bootstrap-responsive.min.css" media="screen" rel="stylesheet" type="text/css">
+  <link href="assets/stylesheets/main.css" media="screen" rel="stylesheet" type="text/css">
   <script src="http://lib.sinaapp.com/js/jquery/1.9.0/jquery.min.js"></script>
  </head>
 
@@ -16,14 +17,19 @@
 
   <div>
     <div class="container">
-    突破繁琐，让你的网站人见人爱
+      <ul>
+        <li>文案1</li>
+        <li>案例1</li>
+        <li>案例2</li>
+      </ul>
     </div>
     
     <div class="container">
+      <div>突破繁琐，让你的网站人见人爱</div>
       <div class="row">
-        <div class="span6">
-          <h4>专业<small>安身立命，走向卓越，专业是火龙果的不二法门</small></h4>
-          <ul>
+        <div class="span6 tr">
+          <h4>专业<br><small>安身立命，走向卓越，专业是火龙果的不二法门</small></h4>
+          <ul class="unstyled">
             <li>超过10年的行业经验</li>
             <li>数百个各种行业实施案例</li>
             <li>精于心，简于形，不停追求更好的态度</li>
@@ -31,9 +37,9 @@
           <div>我们的专业知识可以让您的产品更加激动人心，我们不仅仅是实施者，我们更是建议者，是探路者，但您永远是决定者！</div>         
         </div>
         
-        <div class="span6">        
-          <h4>高效<small></small></h4>
-          <ul>
+        <div class="span6 tl">        
+          <h4>高效<div><small>省时省力不省质，火龙果最高效！</small></div></h4>          
+          <ul class="unstyled">
             <li>7*24小时永不下线的客服</li>
             <li>充满热情，率高极高的工程师</li>
             <li>快速灵活，小而美的制度和文化</li>
@@ -42,18 +48,18 @@
         
         <div class="clearfix"></div>
         
-        <div class="span6">        
-          <h4>性价比<small>最小的资源获得最好的产品和服务——只在火龙果</small></h4>
-          <ul>
+        <div class="span6 tr">        
+          <h4>性价比<br><small>最小的资源获得最好的产品和服务——只在火龙果</small></h4>
+          <ul class="unstyled">
             <li>极少的花费打造顶级互联网公司的产品</li>
             <li></li>
             <li></li>
           </ul>
         </div>
         
-        <div class="span6">        
-          <h4>安全<small>您的担心，我们感同身受</small></h4>
-          <ul>
+        <div class="span6 tl">        
+          <h4>安全<br><small>您的担心，我们感同身受</small></h4>
+          <ul class="unstyled">
             <li>支付宝担保交易</li>
             <li>合同详尽透明</li>
             <li>每天迭代和交付产出物</li>
@@ -63,7 +69,7 @@
     </div>
     
     <div class="container">
-      <dl>
+      <dl class="dl-horizontal">
         <dt>联系电话<dt>
         <dd>13482285699</dd>
         <dt>QQ<dt>
@@ -73,36 +79,37 @@
       </dl>
     </div>
     
-    <div class="container">
-      <div>
-        <h4>建站套餐：3800起</h4>
-        <ul>
-          <li>基于开源cms系统搭建</li>
-          <li>首页及3个内页风格设计</li>
-          <li>3次后续更改服务</li>
-        </ul>
-      </div>
-      
-      <div>
-        <h4>定制套餐：8800起</h4>
-        <ul>
-          <li>基于现有成熟稳定系统定制</li>
-          <li>页面的专业个性化设计</li>
-          <li>8小时候内无限次修改</li>
-        </ul>
-      </div>
-      
-      <div>
-        <h4>完全定制套餐：面议</h4>
-        <ul>
-          <li>专业，富于经验的工程师全程按照你的需求打造</li>
-          <li>所有页面的独特设计</li>
-          <li>8小时候内无限次修改</li>
-        </ul>
-      </div>
-    </div>
+    <div class="container price">
+		<div class="row">
+		  <div class="short span4">
+			<h4>建站套餐：3800起</h4>
+			<ul>
+			  <li>基于开源cms系统搭建</li>
+			  <li>首页及3个内页风格设计</li>
+			  <li>3次后续更改服务</li>
+			</ul>
+		  </div>
+		  
+		  <div class="tall span4">
+			<h4>定制套餐：8800起</h4>
+			<ul>
+			  <li>基于现有成熟稳定系统定制</li>
+			  <li>页面的专业个性化设计</li>
+			  <li>8小时候内无限次修改</li>
+			</ul>
+		  </div>
+		  
+		  <div class="short span4">
+			<h4>完全定制套餐：面议</h4>
+			<ul>
+			  <li>专业，富于经验的工程师全程按照你的需求打造</li>
+			  <li>所有页面的独特设计</li>
+			  <li>8小时候内无限次修改</li>
+			</ul>
+		  </div>	
+		</div>
+	</div>
   </div>
-
   <?php require_once "app/views/common/footer.php"; ?>
  </body>
 </html>
