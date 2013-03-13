3f8aca13876119edd8ffcc93a38be457f4e39fe3
robotzhang <445679586@qq.com>
2013-03-13 22:17:16 +0800

    口号 细化了一下

--- index.php
+++ index.php
@@ -23,47 +23,56 @@
         </li>
       </ul>
     </div>
-    
+
     <div class="container">
-      <div>突破繁琐，让你的网站人见人爱</div>
+      <div class="fs16 tc" style="color: #017EC1; font-size: 22px; margin: 20px 0; "><span>突破</span>繁琐，让您的网站人见人爱</div>
       <div class="row">
-        <div class="span6 tr">
-          <h4>专业<br><small>安身立命，走向卓越，专业是火龙果的不二法门</small></h4>
-          <ul class="unstyled">
-            <li>超过10年的行业经验</li>
-            <li>数百个各种行业实施案例</li>
-            <li>精于心，简于形，不停追求更好的态度</li>
-          </ul> 
-          <div>我们的专业知识可以让您的产品更加激动人心，我们不仅仅是实施者，我们更是建议者，是探路者，但您永远是决定者！</div>         
+        <div class="span3">
+          <div class="why-box">
+              <h4>专业<br><small>戮力不缀，迈向卓越</small></h4>
+              <ul class="unstyled">
+                <li>超过10年的行业经验</li>
+                <li>数百个各种行业实施案例</li>
+                <li>精于心，简于形，追求更好的态度</li>
+              </ul>
+              <div>我们的专业知识可以让您的产品更加激动人心，我们不仅仅是实施者，我们更是建议者，是探路者，但您永远是决定者！</div>
+          </div>
         </div>
         
-        <div class="span6 tl">        
-          <h4>高效<div><small>省时省力不省质，火龙果最高效！</small></div></h4>          
-          <ul class="unstyled">
-            <li>7*24小时永不下线的客服</li>
-            <li>充满热情，率高极高的工程师</li>
-            <li>快速灵活，小而美的制度和文化</li>
-          </ul>
+        <div class="span3">
+            <div class="why-box">
+              <h4>高效<div><small>省时省力不省质，火龙果最高效！</small></div></h4>
+              <ul class="unstyled">
+                <li>7*24小时永不下线的客服</li>
+                <li>充满热情，效率极高的工程师</li>
+                <li>快速灵活，小而美的制度和文化</li>
+              </ul>
+              <div>从沟通到实施，拒绝繁琐的流程，拒绝繁琐的工具和技术，拒绝繁琐的碰面会议，高效是火龙果一贯的追求</div>
+            </div>
         </div>
         
-        <div class="clearfix"></div>
-        
-        <div class="span6 tr">        
-          <h4>性价比<br><small>最小的资源获得最好的产品和服务——只在火龙果</small></h4>
-          <ul class="unstyled">
-            <li>极少的花费打造顶级互联网公司的产品</li>
-            <li></li>
-            <li></li>
-          </ul>
+        <div class="span3">
+            <div class="why-box">
+              <h4>性价比<br><small>最小的资源，最好的产品和服务</small></h4>
+              <ul class="unstyled">
+                <li>相对低的价格</li>
+                <li>绝不冗余的功能</li>
+                <li>性能卓越，体验极致的产品</li>
+              </ul>
+              <div>去除一切不符合需求的功能，我们只对客户的产品负责。相同的价格，顶级互联网产品的极致体验只在火龙果。</div>
+            </div>
         </div>
         
-        <div class="span6 tl">        
-          <h4>安全<br><small>您的担心，我们感同身受</small></h4>
-          <ul class="unstyled">
-            <li>支付宝担保交易</li>
-            <li>合同详尽透明</li>
-            <li>每天迭代和交付产出物</li>
-          </ul>
+        <div class="span3">
+            <div class="why-box">
+              <h4>安全<br><small>您的担心，我们感同身受</small></h4>
+              <ul class="unstyled">
+                <li>支付宝担保交易</li>
+                <li>合同详尽透明</li>
+                <li>每天迭代和交付产出物</li>
+              </ul>
+              <div>精化到每天的产品迭代期，每天您都可以拿到交付物，产品开发不再是“正在进行”的空头话。最大限度保障了项目的安全推进和实施。</div>
+            </div>
         </div>
       </div>
     </div>
