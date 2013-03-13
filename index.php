4a4725103241d626975b6b6791caf70f220521ea
robotzhang <445679586@qq.com>
2013-03-13 22:42:57 +0800

    why重新设计，抛弃box阴影的设计

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
       <div class="fs16 tc" style="color: #017EC1; font-size: 22px; margin: 20px 0; "><span>突破</span>繁琐，让您的网站人见人爱</div>
       <div class="row">
         <div class="span3">
-          <div class="why-box">
+          <div class="why-box personal">
               <h4>专业<br><small>戮力不缀，迈向卓越</small></h4>
               <ul class="unstyled">
                 <li>超过10年的行业经验</li>
@@ -40,11 +40,11 @@
         </div>
         
         <div class="span3">
-            <div class="why-box">
+            <div class="why-box productivity">
               <h4>高效<div><small>省时省力不省质，火龙果最高效！</small></div></h4>
               <ul class="unstyled">
                 <li>7*24小时永不下线的客服</li>
-                <li>充满热情，效率极高的工程师</li>
+                <li>热情，效率极高的工程师</li>
                 <li>快速灵活，小而美的制度和文化</li>
               </ul>
               <div>从沟通到实施，拒绝繁琐的流程，拒绝繁琐的工具和技术，拒绝繁琐的碰面会议，高效是火龙果一贯的追求</div>
@@ -52,7 +52,7 @@
         </div>
         
         <div class="span3">
-            <div class="why-box">
+            <div class="why-box good-price">
               <h4>性价比<br><small>最小的资源，最好的产品和服务</small></h4>
               <ul class="unstyled">
                 <li>相对低的价格</li>
@@ -64,7 +64,7 @@
         </div>
         
         <div class="span3">
-            <div class="why-box">
+            <div class="why-box security">
               <h4>安全<br><small>您的担心，我们感同身受</small></h4>
               <ul class="unstyled">
                 <li>支付宝担保交易</li>
